808ecb9395e5c781723833cbf0d1e4651bc12c93
MykhailenkoAnton <mykhailencko2594@gmail.com>
2024-06-15 16:08:35 +0300

    transform added

--- Game/src/GameApp.cpp
+++ Game/src/GameApp.cpp
@@ -2,6 +2,7 @@
 
 
 #include "imguiOrig/imgui.h"
+#include <glm/gtc/matrix_transform.hpp>
 
 class ExampleLayer : public Entite::Layer
 {
@@ -33,10 +34,10 @@ public:
 		m_SquareVA.reset(Entite::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			0.75f, -0.75f, 0.0f,
-			0.75f, 0.75f, 0.0f,
-			-0.75f, 0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f,
+			0.5f, 0.5f, 0.0f,
+			-0.5f, 0.5f, 0.0f
 		};
 		std::shared_ptr<Entite::VertexBuffer> squareBP; 
 		squareBP.reset(Entite::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
@@ -57,6 +58,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -65,7 +67,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 			
 		)";
@@ -94,13 +96,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 			
 		)";
@@ -144,11 +147,11 @@ public:
 
 		if (Entite::Input::IsKeyPressed(ENT_KEY_A))
 		{
-			m_CameraRotation -= m_CameraRotationSpeed * ts;
+			m_CameraRotation += m_CameraRotationSpeed * ts;
 		}
 		else if (Entite::Input::IsKeyPressed(ENT_KEY_D))
 		{
-			m_CameraRotation += m_CameraRotationSpeed * ts;
+			m_CameraRotation -= m_CameraRotationSpeed * ts;
 		}
 
 		Entite::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
@@ -159,7 +162,17 @@ public:
 
 		Entite::Renderer::BeginScene(m_Camera);
 
-		Entite::Renderer::Submit(m_BlueShader, m_SquareVA);
+		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+		for (int x = 0; x < 20; x++)
+		{
+			for (int i = 0; i < 20; i++)
+			{
+				glm::vec3 pos(i * 0.11f, x * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Entite::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+
 		Entite::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Entite::Renderer::EndScene();
@@ -184,11 +197,10 @@ private:
 	Entite::OrthographicCamera m_Camera;
 
 	glm::vec3 m_CameraPosition;
-	float m_CameraMoveSpeed = 10.0f;
+	float m_CameraMoveSpeed = 5.0f;
 
 	float m_CameraRotation = 0.0f;
-	float m_CameraRotationSpeed = 45.0f;
-
+	float m_CameraRotationSpeed = 180.0f;
 };
 
 
