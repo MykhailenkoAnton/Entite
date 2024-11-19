eece9e476b8cc000a4983f5bcf12b281f133142a
MykhailenkoAnton <mykhailencko2594@gmail.com>
2024-11-19 23:54:10 +0200

    Single Shader 2D Renderer

--- Entite/src/Entite/Renderer/Texture.cpp
+++ Entite/src/Entite/Renderer/Texture.cpp
@@ -6,6 +6,24 @@
 
 namespace Entite {
 
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			ENT_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+
+		case RendererAPI::API::OpenGL:
+			return CreateRef<OpenGLTexture2D>(width, height);
+		default:
+			break;
+		}
+
+
+		ENT_CORE_ASSERT(false, "Unknown RendererAPI");
+		return nullptr;
+	}
+
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch (Renderer::GetAPI())
@@ -14,7 +32,7 @@ namespace Entite {
 			ENT_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
 
 		case RendererAPI::API::OpenGL:
-			return std::make_shared<OpenGLTexture2D>(path);
+			return CreateRef<OpenGLTexture2D>(path);
 		default:
 			break;
 		}
